09606f8e8140ca632137302377f4d3143ff0a924
Enfe-git <mrkzakrzewski@gmail.com>
2020-09-22 17:12:31 +0100

    project/assets restructure

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0) {
+            Invoke("StartFirstLevel", 3f);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public void StartFirstLevel() {
+        SceneManager.LoadScene(1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
